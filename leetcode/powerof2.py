c37a274470c548dca96e1fa7a62bdb9e29c960b8
clancy0614 <88695029+clancy0614@users.noreply.github.com>
2021-12-16 12:59:29 -0500

    Update powerof2.py

--- leetcode/powerof2.py
+++ leetcode/powerof2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[14]:
+## problem: given a positive integer n, return true if n is a power of 2.
 
+## solution 1: if n is odd, return false; if n is even, keep deviding n by 2 until either n = 1 (return true) or n is odd (return false)
+## time complexity: O(log n)
 
 def isPower(n):
     if n == 1:
@@ -17,11 +19,9 @@ def isPower(n):
             if n == 1:
                 return True
 
+## solution 2: same algorithm, but written in a recursive way.
 
-# In[28]:
-
-
-def isPowerRecursion(n):
+def isPower(n):
     
     if n == 1:
         return True
@@ -32,20 +32,27 @@ def isPowerRecursion(n):
     return isPower(n/2)
 
 
-# In[26]:
+## solution 3: start from an initial guess num = 1, keep multiplying num by 2 until num >= n.
+
+def isPower(n):
+        num = 1
+        
+        while num <= n:
+            if num == n:
+                return True
+            num = num * 2
+            
+        return False
 
 
-isPower(8)
+## solution 4: bitwise operation
+## in binary form, n can be written by 0...010...0 and n-1 by 0...001...1, if n is a power of 2. 
+## then we apply & operation on n and n-1, leading to 0...000...0. 
+
+## time complexity: O(1)
 
 
-# In[31]:
-
-
-isPowerRecursion(8)
-
-
-# In[ ]:
-
-
+def isPower(n):
+    return (n & n-1) == 0
 
 
